3b2e2eab56c7d08b31c50473b7a1435ba1a747b3
avisserquinn <a.visser-quinn@hw.ac.uk>
2020-12-15 22:58:10 +0000

    Simplification of visLayout()

--- R/misc.R
+++ R/misc.R
@@ -14,18 +14,128 @@ weightEdges <- function(x, y) {
 
 }
 
-visLayout <- function(edgelist, vInfo, minSpacing = 0, maxSpacing = 100, key) {
+visLayout <- function(edgelist, vInfo, key, spacing) {
 
   require(ggraph)
 
-  #source("functions/functions_internal_vis_layout.R", local = TRUE)
+  vInfo <- vInfo %>% arrange(level, vName)
+  edgelist <- edgelist %>% arrange(layer)
 
-  output <-
-    .internal_horizontalLayout(edgelist = edgelist, vInfo = vInfo) %>%
-    .internal_horizontalLayout_spacing(., minSpacing = minSpacing, maxSpacing = maxSpacing) %>%
-    .internal_radialLayout(horizontalLayout = ., edgelist = edgelist, key = key) %>%
-    .internal_genericLayout(radialLayout = .)
 
+  # DUMMY VERTEX ------------------------------------------------------------
+  VINFO <- vInfo %>% filter(level == 1)
+
+  EDGELIST <- edgelist %>% add_row(.before = 1, layer = "dummyLayer", from = "dummyVertex", to = VINFO$vName)
+
+  VINFO <-
+    vInfo %>%
+    mutate(level = level + 1) %>%
+    add_row(.before = 1, level = 1, levelName = "dummyLevel", vName = "dummyVertex")
+
+  IGRAPH <- EDGELIST %>% select(from, to, weight) %>% graph.data.frame(directed = FALSE)
+
+
+  # SUGIYAMA LAYOUT TEMPLATE ------------------------------------------------
+  LAYOUT <-
+    (IGRAPH %>%
+       layout_with_sugiyama(layers = VINFO$level))$layout %>%
+    as.data.frame %>%
+    setNames(c("x", "y")) %>%
+    slice(-1) %>%
+    cbind(vInfo, .) %>%
+    as_tibble() %>%
+    group_by(level, levelName) %>%
+    mutate(x = scales::rescale(x, to = c(-1,1)),
+           y = -level,
+           pos = 1:n()) %>%
+    ungroup %>%
+    arrange(level, x, y) %>%
+    split(.$level)
+
+
+  # VERTEX SPACING ----------------------------------------------------------
+  LAYOUT <- lapply(LAYOUT, function(i) {
+
+    if(nrow(i) == 1) { dt <- i } else {
+
+      dt <- i
+
+      for(j in 1:(nrow(dt)-1)) {
+
+        diff = abs(dt$x[[j]] - dt$x[[j + 1]])
+
+        if(diff < spacing[[1]]) { dt$x[-(1:j)] <- dt$x[-(1:j)] + (spacing[[1]] - diff) }
+        if(diff > spacing[[2]]) { dt$x[-(1:j)] <- dt$x[-(1:j)] - (diff - spacing[[2]]) }
+
+      }
+
+    }
+
+    return(dt)
+
+  })
+
+
+  # RADIAL LAYOUT -----------------------------------------------------------
+  makeRadial <- function(dataInput, dataKey) {
+
+    midPoint <- 270
+
+    # Convert angles to radian
+    min <- midPoint - dataKey$angle
+    min <- min * pi / 180
+    max <- dataKey$angle + midPoint
+    max <- max * pi / 180
+
+    # S = R0, arc length is equal to radius multiplied by theta
+    minS <- min * dataInput$y[[1]]
+    maxS <- max * dataInput$y[[1]]
+
+    # (Optional) Adjustments to radius
+    R <- dataKey$addR + dataInput$level[[1]]
+
+    # Convert to polar coordinates
+    dataInput %>%
+      mutate(x = scales::rescale(x, to = c(minS, maxS))) %>%
+      mutate(thetaRad2 = x/y,
+             x2 = R * cos(thetaRad2),
+             y2 = R * sin(thetaRad2))
+
+  }
+
+  LAYOUT_GG <- lapply(1:length(LAYOUT), function(x) { makeRadial(LAYOUT[[x]], key[x,]) }) %>% bind_rows()
+
+  LAYOUT_GG <-
+    LAYOUT_GG %>%
+    select(-x, -y) %>%
+    rename(x = x2, y = y2, theta = thetaRad2) %>%
+    create_layout(edgelist, layout = .)
+
+
+  # EXTRACT -----------------------------------------------------------------
+  findEdges <- get_edges("short", collapse = "none")
+
+  edges <-
+    findEdges(LAYOUT_GG) %>%
+    as_tibble %>%
+    select(fromLevel = node1.level,  toLevel = node2.level, layer,
+           from = node1.name, to = node2.name,
+           x = node1.x, y = node1.y, xend = node2.x, yend = node2.y) %>%
+    mutate(layer = factor(layer, levels = edgelist %>% pull(layer) %>% levels))
+
+  findNodes <- get_nodes()
+
+  vertices <-
+    findNodes(LAYOUT_GG) %>%
+    as_tibble %>%
+    select(level, levelName, vName = name, x, y, theta)
+
+
+  # OUTPUT ------------------------------------------------------------------
+  vertices$vName <- gsub("'", '', vertices$vName)
+  edges$to <- gsub("'", '', edges$to)
+
+  output <- list(edges = edges, vertices = vertices)
   return(output)
 
 }
--- R/newFunctions.R
+++ R/newFunctions.R
@@ -7,3 +7,88 @@ findOrphans <- function(edgelist) {
     pull(from)
 
 }
+
+vNetwork <- function(vName, vInfo, edgelist, direction = c("both", "up", "down")) {
+
+  # FUNCTIONS
+  networkDown <- function(vName, vInfo, edgelist) {
+
+    edges <- list()
+    vertices <- list()
+
+    myVertex <- vName
+    myLevel <- vInfo %>% filter(vName %in% myVertex) %>% pull(level)
+
+    vertices[[myLevel]] <- myVertex
+
+    levels <- seq(myLevel, max(edgelist$fromLevel), 1)
+
+    for(i in levels) {
+
+      edges[[i]] <- edgelist %>% filter(fromLevel == i) %>% filter(from %in% vertices[[i]])
+      vertices[[i+1]] <- edges[[i]] %>% pull(to) %>% unique
+
+    }
+
+    vertices <- do.call(c, vertices)
+    vertices <- vInfo %>% filter(vName %in% vertices)
+    edges <- do.call(rbind, edges)
+
+    list(vertices = vertices, edges = edges)
+
+  }
+
+  networkUp <- function(vName, vInfo, edgelist) {
+
+    edges <- list()
+    vertices <- list()
+
+    myVertex <- vName
+    myLevel <- vInfo %>% filter(vName %in% myVertex) %>% pull(level)
+
+    vertices[[myLevel]] <- myVertex
+
+    levels <- seq(myLevel, min(edgelist$toLevel), -1)
+
+    for(i in levels) {
+
+      edges[[i]] <- edgelist %>% filter(toLevel == i) %>% filter(to %in% vertices[[i]])
+      vertices[[i-1]] <- edges[[i]] %>% pull(from) %>% unique
+
+    }
+
+    vertices <- do.call(c, vertices)
+    vertices <- vInfo %>% filter(vName %in% vertices)
+    edges <- do.call(rbind, edges)
+
+    list(vertices = vertices, edges = edges)
+
+  }
+
+
+  # APPLIED
+  edges <- list()
+  vertices <- list()
+
+  if(direction == "both" | direction == "up") {
+
+    tmp <- networkUp(vName, vInfo, edgelist)
+    edges[["up"]] <- tmp$edges
+    vertices[["up"]] <- tmp$vertices
+
+  }
+
+  if(direction == "both" | direction == "down") {
+
+    tmp <- networkDown(vName, vInfo, edgelist)
+    edges[["down"]] <- tmp$edges
+    vertices[["down"]] <- tmp$vertices
+
+  }
+
+  vertices <- do.call(rbind, vertices)
+  edges <- do.call(rbind, edges)
+
+  list(vertices = vertices, edges = edges)
+
+}
